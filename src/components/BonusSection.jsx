26edc9d18a5703b37ee99a2e1c29af0d1054d92d
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2025-03-12 19:54:27 +0500

    Bonuses Completed

--- src/components/BonusSection.jsx
+++ src/components/BonusSection.jsx
@@ -1,6 +1,6 @@
-import './../assets/css/Bonuses.css';
-import CSGOBIG from './../assets/images/CSGOBIG.png';
-import RallLogo from './../assets/images/RALLLogo.png'
+import './../assets/css/homeBonuses.css';
+import './../assets/css/bonusCards.css';
+import { BonusCards } from './BonusCards';
 export let BonusSection = () => {
     return <>
         <section className="bonusSection">
@@ -10,82 +10,7 @@ export let BonusSection = () => {
             </div>
             <div className="bonusBottom">
                 <div className="bonusCardsContainer">
-                    <div className="bonusCard">
-                        <div className="gradient"></div>
-                        <div className="color"></div>
-                        <div className="cardContent">
-                            <div className='imageCont'>
-                                <img className="bonusImage" src={CSGOBIG} alt="Bonus Image" />
-                            </div>
-                            <div className="sponsorHeading">
-                                <span className="line"></span>
-                                <span className="sponsorName">CSGOBIG</span>
-                                <span className="line"></span>
-                            </div>
-                            <div className="bonusPrizeContainer">
-                                <div className="bonusDepositContainer">
-                                    <span>Bonus</span>
-                                    <p>5% Deposit Bonus</p>
-                                </div>
-                                <div className="bonusCodeContainer">
-                                    <span>Code</span>
-                                    <p>"SODIUMS"</p>
-                                </div>
-                            </div>
-                            <button type="button" className="claimBtn">Claim Bonus</button>
-                        </div>
-                    </div>
-                    <div className="bonusCard">
-                        <div className="gradientRed"></div>
-                        <div className="color"></div>
-                        <div className="cardContent">
-                            <div className='imageCont'>
-                                <img className="bonusImage" src={RallLogo} alt="Bonus Image" />
-                            </div>
-                            <div className="sponsorHeading">
-                                <span className="line"></span>
-                                <span className="sponsorName">CSGOROLL</span>
-                                <span className="line"></span>
-                            </div>
-                            <div className="bonusPrizeContainer">
-                                <div className="bonusDepositContainer">
-                                    <span>Bonus</span>
-                                    <p>5% Deposit Bonus</p>
-                                </div>
-                                <div className="bonusCodeContainer redBorder">
-                                    <span>Code</span>
-                                    <p>"SODIUMS"</p>
-                                </div>
-                            </div>
-                            <button type="button" className="claimBtn">Claim Bonus</button>
-                        </div>
-                    </div>
-                    <div className="bonusCard">
-                        <div className="gradient"></div>
-                        <div className="color"></div>
-                        <div className="cardContent">
-                            <div className='imageCont'>
-                                <img className="bonusImage" src={CSGOBIG} alt="Bonus Image" />
-                            </div>
-                            <div className="sponsorHeading">
-                                <span className="line"></span>
-                                <span className="sponsorName">CSGOBIG</span>
-                                <span className="line"></span>
-                            </div>
-                            <div className="bonusPrizeContainer">
-                                <div className="bonusDepositContainer">
-                                    <span>Bonus</span>
-                                    <p>5% Deposit Bonus</p>
-                                </div>
-                                <div className="bonusCodeContainer">
-                                    <span>Code</span>
-                                    <p>"SODIUMS"</p>
-                                </div>
-                            </div>
-                            <button type="button" className="claimBtn">Claim Bonus</button>
-                        </div>
-                    </div>
-
+                    <BonusCards />
                 </div>
             </div>
         </section>
--- src/components/pages/Bonuses.jsx
+++ src/components/pages/Bonuses.jsx
@@ -1,10 +1,30 @@
 import { Navbar } from "../navbar";
 import { BgDesign } from "../bgDesign";
 import { Footer } from '../Footer';
+import { BonusSection } from "../BonusSection";
+import { BonusCards } from "../BonusCards";
+import './../../assets/css/bonuses.css';
+import './../../assets/css/bonusCards.css';
 export let Bonuses = () => {
     return <>
         <Navbar />
         <BgDesign />
+        <section className="bonusSection">
+            <div className="bonusTop">
+                <div className="bonusTextContent">
+                    <div className="bonusHeading">
+                        <h2 className="sectionHeading">Bonuses</h2>
+                        <p className="sectionPara">Claim exclusive Offers with Code "Sodiums"</p>
+                    </div>
+                </div>
+            </div>
+            <div className="bonusBottom">
+                <div className="bonusCardsContainer">
+                    <BonusCards />
+                    <BonusCards />
+                </div>
+            </div>
+        </section>
         <Footer />
     </>
 }
