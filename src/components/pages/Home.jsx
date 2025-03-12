e6198cf72eabfb2e3e3c5fae9e288dca19a81d4d
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2025-03-12 17:59:13 +0500

    Bonus Section Completed

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,14 +1,16 @@
+import './../../assets/css/Bonuses.css';
 import { BgDesign } from "../bgDesign"
 import { Navbar } from "../navbar"
 import { Footer } from "../Footer"
 import { Landing } from "../Landing"
-
+import { BonusSection } from "../BonusSection"
 export let Home = () => {
     return <>
         <BgDesign />
         <Navbar />
         <main className="main">
             <Landing />
+            <BonusSection />
         </main>
         <Footer />
     </>
