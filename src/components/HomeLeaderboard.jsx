d72ac855a944bf4a4f6878e9733da378d281a05e
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2025-03-17 14:40:22 +0500

    home  leaderbaord bug fixed

--- src/components/HomeLeaderboard.jsx
+++ src/components/HomeLeaderboard.jsx
@@ -1,14 +1,22 @@
 import { LeaderBoardCards } from "./LeaderBoardCards";
 import { LeaderboardTime } from "./LeaderboardTime";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 export let HomeLeaderboard = () => {
+    let [activePartner, setActivePartner] = useState(
+        {
+            clash: false,
+            big: true,
+            rain: false,
+        }
+    );
     return (<>
         <section className="leaderboardHomeSection">
             <div className="sectionTop">
                 <h2 className="sectionHeading" data-aos='zoom-in-right'>Leaderboard</h2>
                 <p className="sectionPara" data-aos='zoom-in-left'>Compete in Wager Races and win huge Prizes!</p>
             </div>
-            <LeaderBoardCards />
+            <LeaderBoardCards activePartner={activePartner} />
             <LeaderboardTime />
             <button data-aos='zoom-in' className="leaderboardsBtn"><Link to='/leaderboard'>Leaderboards</Link></button>
         </section>
